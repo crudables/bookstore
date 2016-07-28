fc13f685353c3d2d76360f42df0c6100b76a49c8
ables <crudables@yahoo.com>
2016-07-28 15:43:23 +0100

    push

--- BookStore/bookstore/models.py
+++ BookStore/bookstore/models.py
@@ -1,13 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 class Book(models.Model):# Create your models here.
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     publisher = models.CharField(max_length=100)
-    price = models.IntegerField(default=0.00)
-    released_date = models.DateTimeField
+    price = models.DecimalField(max_digits=5, decimal_places=2,default=0.00)
+    former_price = models.DecimalField(max_digits=5, decimal_places=2,default=0.00)
+    released_date = models.DateTimeField(default=timezone.now)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=30)
+    isbn = models.CharField(primary_key=True,max_length=15, default="0000000000")
+    picture = models.ImageField(default="")
+    about = models.TextField(default="No information provided for this book")
+    language = models.CharField(max_length=20,default="English")
     
--- BookStore/bookstore/views.py
+++ BookStore/bookstore/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import  Book
 
 # Create your views here.
 def post_list(request):
-    return render(request,'bookstore/homepage.html',{})
+    books = Book.objects.all()
+    return render(request,'bookstore/homepage.html',{'books':books})
